3cae62321f30d4bfd73aad473b3ac31a714de319
TaniaGitHub29 <37541766+TaniaGitHub29@users.noreply.github.com>
2022-04-22 13:02:15 +0200

    prova2

--- src/it/develhope/exercisestatic/Tester.java
+++ src/it/develhope/exercisestatic/Tester.java
@@ -8,14 +8,13 @@ package it.develhope.exercisestatic;
 
 public class Tester {
 
-    public static void main(String[] args) {
+    public static void main(String [] args) {
 
         //declaration and creation of 2 employees
         Employee employee1, employee2;
         employee1=new Employee("Tania", "Ielpo", "via Dante,13");
         employee2=new Employee("Stefano","Rossi", "via Roma,23");
 
-        employee1.surname="iekpo";
         //declaration and creation of 2 badges and relative association with employees
         Badge badge1, badge2;
         badge1= new Badge(employee1);
